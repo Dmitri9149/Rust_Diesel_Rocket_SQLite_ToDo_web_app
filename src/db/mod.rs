74933f637b0e25d459d01c582c0b068fdc79b6e9
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-15 21:06:34 +0200

    query_task is added

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -19,20 +19,7 @@ pub fn create_task(connection: &SqliteConnection, title: &str) {
 
 }
 
-fn show_tasks(args: &[String]) {
-    if args.len()  > 0 {
-        println!("show: unexpected argument");
-        help();
-        return;
-    }
-    let conn = establish_conection();
-    println!("TASKS\n--------");
-    for task in query_task(&conn) {
-        println!("{}", task.title);
-    }
-}
-
-
-
-
-
+pub fn query_task(connection: &SqliteConnection) -> Vec<models::task>
+    schema::task::table
+    .load::<models::Task>(connection)
+    .expect("Error loading tasks")
