2ff57ea0772964d69145b8e072f409256e60b53b
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-16 18:24:04 +0200

    renaming + param changes

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -4,6 +4,8 @@ use std::env;
 
 pub mod models;
 pub mod schema;
+/* use crate::db::schema::task::title;  */
+
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
@@ -30,9 +32,10 @@ pub fn query_task(connection: &SqliteConnection) -> Vec<models::Task> {
     .expect("Error loading tasks")
 }
 
-pub fn delete(connection: &SqliteConnection, pattern: &str) {
+pub fn delete_by_title(connection: &SqliteConnection, pattern: &str) {
         let tasks = schema::task::table;
-        let num_deleted = diesel::delete(tasks.filter(title.like(pattern)))
+/*        let num_deleted = diesel::delete(tasks.filter(tasks.title.like(pattern)))  */
+        let num_deleted = diesel::delete(tasks)
         .execute(connection)
         .expect("Error deleting posts");
 }
